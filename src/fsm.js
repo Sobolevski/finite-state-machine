c7577a077bad395dd612c94c0d301bfb58d12a55
sobolevski <semion.sobolevski@gmail.com>
2017-02-05 22:01:18 +0300

    finite-state-machihe/complete: maybe complete..

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,61 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config)
+            throw new Error();
+
+        this.states = config.states;
+        this.stack = [];
+        this.stack.push(config.initial);
+        this.initial = config.initial;
+        this.deleted = [];
+        this.is_initial = true;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.stack[this.stack.length - 1];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (this.states[state] == undefined)
+            throw new Error();
+        this.stack.push(state);
+        this.is_initial = false;
+        this.deleted = [];
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        var new_state;
+        new_state = this.states[this.stack[this.stack.length - 1]].transitions[event];
+        if (new_state == undefined)
+            throw new Error();
+        this.stack.push(new_state);
+        this.is_initial = false;
+        this.deleted = [];
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.stack = []
+        this.stack.push(this.initial);
+        this.is_initial = true;
+        this.deleted = [];
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,28 +65,65 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (event == undefined) {
+            var keys = [];
+            for (var key in this.states)
+                keys.push(key);
+            return keys;
+        } else {
+            var values = [];
+            for (var key in this.states) {
+                if (this.states[key].transitions[event] == undefined)
+                    continue;
+                values.push(key)
+            }
+            return values;
+        }
+
+
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.is_initial || this.stack.length <= 1)
+            return false;
+        else {
+            this.deleted.push(this.stack[this.stack.length - 1]);
+            this.stack.pop();
+            return true;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.deleted == false)
+            return false;
+        else {
+            this.stack.push(this.deleted[this.deleted.length - 1])
+            this.deleted.pop();
+            return true;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        var tmp_state = this.stack[this.stack.length - 1];
+        this.stack = [];
+        this.deleted = [];
+        this.stack.push(tmp_state);
+        this.is_initial = true;
+    }
 }
 
 module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
